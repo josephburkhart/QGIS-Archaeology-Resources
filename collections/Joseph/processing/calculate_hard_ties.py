5f25d8190be2bc40a2e6e6e0937f5dce4d227b3b
Joseph Burkhart <joseph.burkhart1@comcast.net>
2022-10-10 12:24:47 -0700

    Shorten and homogenize variable names

--- collections/Joseph/processing/calculate_hard_ties.py
+++ collections/Joseph/processing/calculate_hard_ties.py
@@ -5,7 +5,7 @@ from qgis.core import *
 from qgis.gui import *
 import math as m
 
-def true_north_bearing(pointfeature, source_crs, project_instance):
+def true_north_bearing(pointfeature, source_crs, proj_inst):
     """
     Calculates the bearing (counter-clockwise) of True North relative to Grid 
     North at a specified point within a specified crs.
@@ -15,7 +15,7 @@ def true_north_bearing(pointfeature, source_crs, project_instance):
     
     # Define CRS transformation
     dest_crs = QgsCoordinateReferenceSystem(4326)
-    tr = QgsCoordinateTransform(source_crs, dest_crs, project_instance)
+    tr = QgsCoordinateTransform(source_crs, dest_crs, proj_inst)
     
     # Clone geometry and transform it
     geom2 = QgsGeometry(geom)
@@ -25,7 +25,7 @@ def true_north_bearing(pointfeature, source_crs, project_instance):
     geom2.translate(0,0.001)
     
     # Transform Geometry back to source CRS
-    tr = QgsCoordinateTransform(dest_crs, source_crs, project_instance)
+    tr = QgsCoordinateTransform(dest_crs, source_crs, proj_inst)
     geom2.transform(tr)
     
     # Calculate angle between the two points
@@ -41,14 +41,14 @@ def true_north_bearing(pointfeature, source_crs, project_instance):
     
     return angle
     
-def measure_distance(feat1, feat2, project_instance):
+def measure_distance(feat1, feat2, proj_inst):
     """
     Returns ellipsoidal distance between 2 point features in the project CRS.
     """
     # Configure the distance measurement
-    proj_crs = project_instance.crs()
+    proj_crs = proj_inst.crs()
     distance = QgsDistanceArea()
-    distance.setSourceCrs(proj_crs, project_instance.transformContext())
+    distance.setSourceCrs(proj_crs, proj_inst.transformContext())
     distance.setEllipsoid(proj_crs.ellipsoidAcronym())
     
     # Measure distance
@@ -60,7 +60,7 @@ def measure_distance(feat1, feat2, project_instance):
     
     return m
     
-def calculate_hard_tie(ref_pt, ref_pt_name_field, feat_pt, project_instance):
+def calculate_hard_tie(ref_pt, ref_pt_name_field, feat_pt, proj_inst):
     """
     Determines the distance and compass bearing (relative to True North) from
     a reference point to a feature point using the project CRS.
@@ -69,10 +69,10 @@ def calculate_hard_tie(ref_pt, ref_pt_name_field, feat_pt, project_instance):
     from <ref_pt name>'
     """
     # Determine project CRS
-    proj_crs = project_instance.crs()
+    proj_crs = proj_inst.crs()
     
     # Calculate distance to nearest cm
-    distance = round(measure_distance(ref_pt, feat_pt, project_instance), 2)
+    distance = round(measure_distance(ref_pt, feat_pt, proj_inst), 2)
     
     # Calculate bearing to nearest 0.1 degrees
     p1 = ref_pt.geometry().asPoint()
@@ -84,7 +84,7 @@ def calculate_hard_tie(ref_pt, ref_pt_name_field, feat_pt, project_instance):
     y2 = p2.y()
     
     bearing_from_gn = QgsGeometryUtils.lineAngle(x1, y1, x2, y2)*(180/(m.pi))
-    tn_bearing = true_north_bearing(feat_pt, proj_crs, project_instance)
+    tn_bearing = true_north_bearing(feat_pt, proj_crs, proj_inst)
     bearing_from_tn = round(bearing_from_gn + (360 - tn_bearing), 1)
     
     # Get Reference Point Name
@@ -95,7 +95,7 @@ def calculate_hard_tie(ref_pt, ref_pt_name_field, feat_pt, project_instance):
     
     return description
 
-def create_hard_tie(from_pt, to_pt, desc,  from_text, to_text, layer, project_instance):
+def create_hard_tie(from_pt, to_pt, desc,  from_txt, to_txt, layer, proj_inst):
     """
     Create polyline feature representing a hard-tie
     """
@@ -109,14 +109,14 @@ def create_hard_tie(from_pt, to_pt, desc,  from_text, to_text, layer, project_in
     
     #Set the new feature's attributes
     feature.setFields(layer.fields())
-    feature.setAttribute("From", from_text)
+    feature.setAttribute("From", from_txt)
     feature.setAttribute("Desc", desc)
-    feature.setAttribute("To", to_text)
+    feature.setAttribute("To", to_txt)
     
     #Update the data file and add the updated data to the QGIS project
     layer.dataProvider().addFeatures([feature])     #add output feature to layer's data provider
     layer.updateExtents()                           #update layer extents based on the new info in the data provider
-    project_instance.addMapLayers([layer])     #add updated layer to the qgis project instance
+    proj_inst.addMapLayers([layer])     #add updated layer to the qgis project instance
 
 def reproject_layer(layer, out_crs, proj_inst, add_to_iface=False):
     """
@@ -156,29 +156,29 @@ def reproject_layer(layer, out_crs, proj_inst, add_to_iface=False):
     return layer_rpj
 
 # User inputs
-feature_layer_name = "New scratch layer"
-feature_id_field = "name"
-reference_layer_name = "ReferencePoints2"
-reference_id_field = "RPID"
+feat_layer_name = "New scratch layer"
+feat_id_field = "name"
+ref_layer_name = "ReferencePoints2"
+ref_id_field = "RPID"
 
 # Project variables
-project_instance =  QgsProject.instance()
-proj_crs = project_instance.crs()
+proj_inst =  QgsProject.instance()
+proj_crs = proj_inst.crs()
 proj_crs_code = str(proj_crs)[-10:-1]
 
 # Get feature layer
-feature_layer = project_instance.mapLayersByName(feature_layer_name)[0]
+feature_layer = proj_inst.mapLayersByName(feat_layer_name)[0]
 
 # Get reference point layer
-reference_layer = project_instance.mapLayersByName(reference_layer_name)[0]
+reference_layer = proj_inst.mapLayersByName(ref_layer_name)[0]
 
 # Make sure feature and reference point layers are in the project CRS
 # (reproject if necessary)
 if reference_layer.crs() != proj_crs:
     print("Warning: reference point layer not in project crs. Reprojecting and continuing...")
-    select_id = reference_layer.selectedFeatures()[0][reference_id_field]
+    select_id = reference_layer.selectedFeatures()[0][ref_id_field]
     reference_layer = reproject_layer(reference_layer, proj_crs, proj_inst)
-    reference_layer.selectByExpression(f"\"{reference_id_field}\" = '{select_id}'")
+    reference_layer.selectByExpression(f"\"{ref_id_field}\" = '{select_id}'")
 
 if feature_layer.crs() != proj_crs:
     print("Warning: feature layer not in project crs. Reprojecting and continuing...")
@@ -190,7 +190,7 @@ features = feature_layer.getFeatures()
 
 # Get reference points
 ref_pt = reference_layer.selectedFeatures()[0]
-ref_pt_name = ref_pt[reference_id_field]
+ref_pt_name = ref_pt[ref_id_field]
 
 # Create output layer
 url = f"Linestring?crs={proj_crs_code}&field=To:string(100)&field=From:string(100)&field=Desc:string(200)"
@@ -198,7 +198,7 @@ output_layer = QgsVectorLayer(url, f"Hard-ties from {ref_pt_name}", "memory")
 
 # Calculate hard-ties
 for f in features:
-    desc = calculate_hard_tie(ref_pt, reference_id_field, f, project_instance)
-    from_text = ref_pt_name
-    to_text = f[feature_id_field]
-    create_hard_tie(ref_pt, f, desc, from_text, to_text, output_layer, project_instance)
+    desc = calculate_hard_tie(ref_pt, ref_id_field, f, proj_inst)
+    from_txt = ref_pt_name
+    to_txt = f[feat_id_field]
+    create_hard_tie(ref_pt, f, desc, from_txt, to_txt, output_layer, proj_inst)
